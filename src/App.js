3a1b0409622d511e829be8e12f8502a70f8d346b
HARlB0 <hama4342@naver.com>
2021-12-01 17:59:56 +0900

    21.12.01 todolist UI complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import add from './img/add.png';
+import check from './img/check.png';
+import delete_button from './img/delete.png';
+import pencil from './img/pencil.png';
+import rec from './img/rec.png';
 
 function App() {
+  const [button, setButton] = useState(false);
+
+  const todo_content = () => {
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg">
+      <div className="todolist">
+        <div className="list_box">
+          <div className="title">
+            <h1>TO DO LIST</h1>
+          </div>
+          <div className="add">
+            <div className="input_box">
+              <input />
+            </div>
+            <div className="add_button">
+              <img src={add} />
+            </div>
+          </div>
+          <div className="body">
+            <div className="content">
+              <div className="check">
+                <div className="check_img" onClick={() => { setButton(!button) }}>
+                  {button == false ?
+                    <img src={rec} />
+                    :
+                    <img src={check} />
+                  }
+                </div>
+                <ul>
+                  <li>간지나게 숨쉬기</li>
+                </ul>
+              </div>
+              <div className="modify">
+                <img id="pencil" src={pencil} />
+                <img src={delete_button} />
+              </div>
+            </div>
+            <div className="content">
+              <div className="check">
+                <div className="check_img" onClick={() => { setButton(!button) }}>
+                  {button == false ?
+                    <img src={rec} />
+                    :
+                    <img src={check} />
+                  }
+                </div>
+                <ul>
+                  <li>간지나게 숨쉬기</li>
+                </ul>
+              </div>
+              <div className="modify">
+                <img id="pencil" src={pencil} />
+                <img src={delete_button} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
